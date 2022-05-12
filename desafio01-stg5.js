7fce82c896f9981ed26f5c7bdc0dc149d5e4bace
AndressaR <dre.rodrigues27@gmail.com>
2022-05-12 10:01:37 -0300

    changing: function music and click

--- desafio01-stg5.js
+++ desafio01-stg5.js
@@ -11,20 +11,12 @@ let main = document.querySelector("main")
 let buttonSunMode = document.querySelector(".sunMode")
 let buttonDarkMode = document.querySelector(".darkMode")
 let sectionRight = document.querySelector(".right")
-let numberDarkMode = document.querySelector(".timer")
+let timer = document.querySelector(".timer")
 let fillPlay = document.querySelector(".fillPlay")
 let fillStop = document.querySelector(".fillStop")
 let fillPlus = document.querySelector(".fillPlus")
 let fillLess = document.querySelector(".fillLess")
-
-// let florestaCard1 = document.querySelector(".florestaCard1")
-// let florestaCard2 = document.querySelector(".florestaCard2")
-// let chuvaCard1 = document.querySelector(".chuvaCard1")
-// let chuvaCard2 = document.querySelector(".chuvaCard2")
-// let cafeteriaCard1 = document.querySelector(".cafeteriaCard1")
-// let cafeteriaCard2 = document.querySelector(".cafeteriaCard2")
-// let lareiraCard1 = document.querySelector(".lareiraCard1")
-// let lareiraCard2 = document.querySelector(".lareiraCard2")
+let cardVolume = document.querySelector(".cardVolume")
 
 
 let minutesDisplay = document.querySelector(".minutes")
@@ -32,48 +24,48 @@ let secondsDisplay = document.querySelector(".seconds")
 
 let putMinutes = Number(minutesDisplay.textContent)
 
-function updateTime(putMinutes, putSeconds){
-    minutesDisplay.textContent = String(putMinutes).padStart(2,"0")
-    secondsDisplay.textContent = String(putSeconds).padStart(2,"0")
+function updateTime(putMinutes, putSeconds) {
+    minutesDisplay.textContent = String(putMinutes).padStart(2, "0")
+    secondsDisplay.textContent = String(putSeconds).padStart(2, "0")
 }
-    
+
 function countdown(fromButtonClick) {
 
     if (fromButtonClick) {
         putMinutes = prompt("Qual o tempo que você deseja colocar? Digite apenas números") || 0
         updateTime(putMinutes, 0)
     }
-    
-    timerTimeOut = setTimeout(function() {
+
+    timerTimeOut = setTimeout(function () {
         let setseconds = Number(secondsDisplay.textContent)
         let setminutes = Number(minutesDisplay.textContent)
-        
-        if (setminutes <=0 && setseconds <=0){ 
+
+        if (setminutes <= 0 && setseconds <= 0) {
             window.alert("O tempo acabou")
             return
-            
+
         }
 
-        if (setseconds <= 0){
+        if (setseconds <= 0) {
             setseconds = 5
-            minutesDisplay.textContent = String(setminutes -1).padStart(2, "0")
+            minutesDisplay.textContent = String(setminutes - 1).padStart(2, "0")
         }
-        
-        secondsDisplay.textContent = String(setseconds -1).padStart(2, "0")
-        
+
+        secondsDisplay.textContent = String(setseconds - 1).padStart(2, "0")
+
         countdown(false)
-    
+
     }, 1000)
 }
 
 buttonPlay.addEventListener('click', () => countdown(true))
-    
 
-buttonStop.addEventListener('click', function() {
+
+buttonStop.addEventListener('click', function () {
     let agree = confirm("Tem certeza que deseja finalizar o Timer?")
 
     if (agree) {
-        updateTime (0, 0)
+        updateTime(0, 0)
     }
     else {
         clearTimeout(timerTimeOut)
@@ -81,134 +73,128 @@ buttonStop.addEventListener('click', function() {
     }
 })
 
-buttonPlus.addEventListener('click', function() {
-    updateTime(Number(minutesDisplay.textContent) +5, 0)
+buttonPlus.addEventListener('click', function () {
+    updateTime(Number(minutesDisplay.textContent) + 5, 0)
 })
 
-buttonLess.addEventListener('click', function() {
-    updateTime(Number(minutesDisplay.textContent) -5, 0)
+buttonLess.addEventListener('click', function () {
+    updateTime(Number(minutesDisplay.textContent) - 5, 0)
 })
 
-// const cardFlorestaAudio = new Audio ("Floresta.wav")
-// const cardChuvaAudio = new Audio ("Chuva.wav")
-// const cardCafeteriaAudio = new Audio ("Cafeteria.wav")
-// const cardLareiraAudio = new Audio ("Lareira.wav")
-
-
-let audio = new Audio("floresta.wav")
+let audio = new Audio('')
 let card = 'floresta'
 
-cardFloresta.addEventListener('click', function(){
+cardFloresta.addEventListener('click', function () {
     playMusic("floresta")
-    changeColor('floresta')
+    changeColor("floresta")
 })
 
-cardChuva.addEventListener('click', function(){
+cardChuva.addEventListener('click', function () {
     playMusic("chuva")
     changeColor('chuva')
 })
 
-cardCafeteria.addEventListener('click', function(){
+cardCafeteria.addEventListener('click', function () {
     playMusic("cafeteria")
     changeColor('cafeteria')
 })
 
-cardLareira.addEventListener('click', function(){
+cardLareira.addEventListener('click', function () {
     playMusic("lareira")
     changeColor('lareira')
 })
 
-function playMusic (name){
+function playMusic(name) {
     const mapa = {
         floresta: "Floresta.wav",
         chuva: "Chuva.wav",
         cafeteria: "Cafeteria.wav",
         lareira: "Lareira.wav",
     }
-    audio.pause()
-    audio = new Audio (mapa[name])
+    audio.src = mapa[name]
     audio.play()
 }
 
+function changeColor (namecard) {
+    document.querySelector('.floresta').classList.remove('clicked')
+    document.querySelector('.chuva').classList.remove('clicked')
+    document.querySelector('.cafeteria').classList.remove('clicked')
+    document.querySelector('.lareira').classList.remove('clicked')
 
-function changeColor (name){
-    document.querySelector(`.${card}Card1`).setAttribute("fill", "#E1E1E6")
-    document.querySelector(`.${card}Card2`).setAttribute("fill", "#323238")
-
-    card = name
-    document.querySelector(`.${name}Card1`).setAttribute("fill", "#02799D")
-    document.querySelector(`.${name}Card2`).setAttribute("fill", "white")
-    document.querySelector(`.${name}Vol1`).setAttribute("fill", "white")
-    document.querySelector(`.${name}Vol2`).setAttribute("fill", "white")
+    document.querySelector(`.${namecard}`).classList.add('clicked')
 }
 
-buttonSunMode.addEventListener('click', function(){
+buttonSunMode.addEventListener('click', function () {
     changeBackgroundcolor('black')
     buttonSunMode.classList.add('hide')
     buttonDarkMode.classList.remove('hide')
-    darkmode('floresta', "white", "#29292E")
-    darkmode('chuva', "white","#29292E")
-    darkmode('cafeteria', "white","#29292E")
-    darkmode('lareira', "white", "#29292E")
     controlsdDarkMode('numberDarkMode')
- 
-
+    darkmode()
 })
 
-function darkmode(name, color, bgcolor){
-    card = name
-    document.querySelector(`.${name}Card1`).setAttribute("fill", `${bgcolor}`)
-    document.querySelector(`.${name}Card2`).setAttribute("fill", `${color}`)
-    document.querySelector(`.${name}Vol1`).setAttribute("fill", `${color}`)
-    document.querySelector(`.${name}Vol2`).setAttribute("fill", `${color}`)
+function darkmode() {
+    const cards = document.querySelectorAll(".card")
+
+    for (const card of cards) {
+        card.classList.add("dark")
+    }
+
+    const cardsVolume = document.querySelectorAll(".cardVolume")
+
+    for( const cardVolume of cardsVolume){
+        cardVolume.classList.add("dark")
+    }
+
+    const buttonControlsDark = document.querySelectorAll(".buttonControls > button")
+
+    for( const buttonControls of buttonControlsDark){
+        buttonControls.classList.add("numberDarkMode")
+    }
 }
 
-function changeBackgroundcolor (color){
+function lightmode() {
+    const cards = document.querySelectorAll(".card")
+
+    for (const card of cards) {
+        card.classList.remove("dark")
+    }
+
+    const cardsVolume = document.querySelectorAll(".cardVolume")
+
+    for( const cardVolume of cardsVolume){
+        cardVolume.classList.remove("dark")
+    }
+
+    const buttonControlsDark = document.querySelectorAll(".buttonControls > button")
+
+    for( const buttonControls of buttonControlsDark){
+        buttonControls.classList.remove("numberDarkMode")
+    }
+}
+
+function changeBackgroundcolor(color) {
     body.style.backgroundColor = `${color}`
     main.style.backgroundColor = `${color}`
     sectionRight.style.backgroundColor = `${color}`
 }
 
-function controlsdDarkMode (buttonClass){
-        numberDarkMode.classList.add(`${buttonClass}`)
-        fillPlay.classList.add(`${buttonClass}`)
-        fillStop.classList.add(`${buttonClass}`)
-        fillPlus.classList.add(`${buttonClass}`)
-        fillLess.classList.add(`${buttonClass}`)
+function controlsdDarkMode(buttonClass) {
+    timer.classList.add(`${buttonClass}`)
 }
 
-function controlsSunMode (buttonClass){
-    numberDarkMode.classList.remove(`${buttonClass}`)
-        fillPlay.classList.remove(`${buttonClass}`)
-        fillStop.classList.remove(`${buttonClass}`)
-        fillPlus.classList.remove(`${buttonClass}`)
-        fillLess.classList.remove(`${buttonClass}`)
+function controlsSunMode(buttonClass) {
+    timer.classList.remove(`${buttonClass}`)
 }
 
-buttonDarkMode.addEventListener('click', function(){
+buttonDarkMode.addEventListener('click', function () {
     controlsSunMode('numberDarkMode')
     changeBackgroundcolor('white')
     body.style.backgroundColor = '#E5E5E5'
     buttonSunMode.classList.remove('hide')
     buttonDarkMode.classList.add('hide')
-    darkmode('floresta', "#323238", '#E5E5E5')
-    darkmode('chuva', "#323238", '#E5E5E5')
-    darkmode('cafeteria', "#323238", '#E5E5E5')
-    darkmode('lareira', "#323238", '#E5E5E5')
+    lightmode()
 })
 
-let rects = document.querySelectorAll('rect');
-for (var i = 0; i < rects.length; i++) {
-    rects[i].addEventListener('mousedown', mousedown);
+function changeVolume(value){
+    audio.volume = parseFloat(value)
 }
-
-shape.onmousedown = function(evt) {
-    var evt = evt || window.event;
-    ddData.element = evt.target || evt.srcElement;
-    ddData.initialX = evt.clientX;
-    ddData.initialY = evt.clientY;
-    ddData.originalX = parseFloat(
-        ddData.element.getAttributeNS(null, "cx"));
-    ddData.originalY = parseFloat(
-        ddData.element.getAttributeNS(null, "cy"));
-};
